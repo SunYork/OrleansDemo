ef23fb238311ebbf31ace5f790754a590c5eceb0
y-z-f <976691141@qq.com>
2016-04-11 16:15:18 +0800

    基于ASP.NET的Demo

---
+++ OrleansWebHost/OrleansWebInterface/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orleans;
+
+namespace OrleansWebInterface
+{
+	public interface IUserService : IGrainWithIntegerKey
+	{
+		Task<User> GetUserById(int id);
+		Task<List<User>> GetUserList();
+		Task<User> GetUserByName(string userName);
+	}
+}
